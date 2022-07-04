dc17eb4c69e6abb70568cafd6ecdc9b5da24cb0f
Yonghyun Kwon <91252737+Yonghyunkkk@users.noreply.github.com>
2022-07-04 22:29:34 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   <Router>
     <Routes>
       <Route path="/movie/:id" element={<Detail />} />
-      <Route path="/" element={<Home />} />
+      <Route path={process.env.PUBLIC_URL + "/"} element={<Home />} />
       <Route path="/movie/TopRating" element={<TopRating />} />
       <Route path="/movie/Animation" element={<Animation />} />
       <Route path="/movie/Action" element={<Action />} />
